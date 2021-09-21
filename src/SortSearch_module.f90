1cc4c9ba0178293f01c00ece25c43592b448ed4b
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-09-21 10:17:12 +0200

    indent code lines

--- src/SortSearch_module.f90
+++ src/SortSearch_module.f90
@@ -72,82 +72,69 @@ end subroutine
 
 
 subroutine Merge(A,NA,B,NB,C,NC)
+    implicit none
+    integer, intent(in) :: NA,NB,NC         ! Normal usage: NA+NB = NC
+    type(point), intent(in out) :: A(NA)        ! B overlays C(NA+1:NC)
+    type(point), intent(in)     :: B(NB)
+    type(point), intent(in out) :: C(NC)
 
-!use point_module
-implicit none
-integer, intent(in) :: NA,NB,NC         ! Normal usage: NA+NB = NC
-type(point), intent(in out) :: A(NA)        ! B overlays C(NA+1:NC)
-type(point), intent(in)     :: B(NB)
-type(point), intent(in out) :: C(NC)
+    integer :: I,J,K
 
-integer :: I,J,K
-
-I = 1; J = 1; K = 1;
-do while(I <= NA .and. J <= NB)
-if (A(I) <= B(J)) then
-C(K) = A(I)
-I = I+1
-else
-C(K) = B(J)
-J = J+1
-endif
-K = K + 1
-enddo
-do while (I <= NA)
-C(K) = A(I)
-I = I + 1
-K = K + 1
-enddo
-return
-
+    I = 1; J = 1; K = 1;
+    do while(I <= NA .and. J <= NB)
+    if (A(I) <= B(J)) then
+        C(K) = A(I)
+        I = I+1
+    else
+        C(K) = B(J)
+        J = J+1
+    endif
+    K = K + 1
+    enddo
+    do while (I <= NA)
+    C(K) = A(I)
+    I = I + 1
+    K = K + 1
+    enddo
+    return
 end subroutine merge
 
 recursive subroutine MergeSort(A,N,T)
+    implicit none
+    integer, intent(in) :: N
+    type(point), dimension(N), intent(in out) :: A
+    type(point), dimension((N+1)/2), intent (out) :: T
+    type(point) :: V
+    integer :: NA,NB
 
-!use point_module
-implicit none
-integer, intent(in) :: N
-type(point), dimension(N), intent(in out) :: A
-type(point), dimension((N+1)/2), intent (out) :: T
-type(point) :: V
-integer :: NA,NB
+    if (N < 2) return
+    if (N == 2) then
+        if (A(1) > A(2)) then
+            V = A(1)
+            A(1) = A(2)
+            A(2) = V
+        endif
+        return
+    endif
+    NA=(N+1)/2
+    NB=N-NA
 
-if (N < 2) return
-if (N == 2) then
-if (A(1) > A(2)) then
-V = A(1)
-A(1) = A(2)
-A(2) = V
-endif
-return
-endif
-NA=(N+1)/2
-NB=N-NA
+    call MergeSort(A,NA,T)
+    call MergeSort(A(NA+1),NB,T)
 
-call MergeSort(A,NA,T)
-call MergeSort(A(NA+1),NB,T)
-
-if (A(NA) > A(NA+1)) then
-T(1:NA)=A(1:NA)
-call Merge(T,NA,A(NA+1),NB,A,N)
-endif
-return
-
+    if (A(NA) > A(NA+1)) then
+        T(1:NA)=A(1:NA)
+        call Merge(T,NA,A(NA+1),NB,A,N)
+    endif
 end subroutine MergeSort
 
 subroutine MergeSortSR(n,a)
-!use point_module
-implicit none
-integer, intent(in) :: n
-type(point),intent(in out) :: a(n)
-type(point) :: temp((n+1)/2)
+    implicit none
+    integer, intent(in) :: n
+    type(point),intent(in out) :: a(n)
+    type(point) :: temp((n+1)/2)
 
-call MergeSort(a,n,temp)
-
-!write(*,*) 'Sorted!'
-!do i=1,n
-! write(56,'(20f12.4)') A(i)%x, A(i)%y
-!end do
+    call MergeSort(a,n,temp)
 
 end subroutine MergeSortSR
 
