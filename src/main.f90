cefe268e046cb950b293d525ef43ad60fe966dac
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2021-09-23 12:48:25 +0200

    init Quadtree-Halfedge

--- src/main.f90
+++ src/main.f90
@@ -4,5 +4,28 @@ program quadtree_main
     use Qtree_module
     
     implicit none
+    type(Qtree), pointer :: Q3 => null()
+    type(QtreeList), pointer :: QtrList => null()
+    real(8) :: pointsArray(4,2)
+    real(8) :: seedsArray(2,2)
 
+    ! A square of side length 10
+    pointsArray(1,:) = [ 0, 0]
+    pointsArray(2,:) = [10, 0]
+    pointsArray(3,:) = [10,10]
+    pointsArray(4,:) = [ 0,10]
+
+    seedsArray(1,:) = [ 2, 2]
+    seedsArray(2,:) = [ 3, 3]
+
+    ! <QUADTREE>
+    ! Quadtree decomposition
+    ! Initilize
+    allocate(Q3)
+    call QtrInit(Q3, 4, pointsArray)
+
+    open(unit=55, file='./mtlb/vertices0.txt', status='unknown')
+        call QtrPrintLeaves(55, Q3)
+    close(unit=55)
+    
 end program quadtree_main
