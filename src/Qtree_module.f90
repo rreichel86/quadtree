19e0b369d4be7fd00d530cfde2edbe37685841ea
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2023-02-19 22:43:23 +0100

    extract Qtree_data module to Qtree_data.f90

--- src/Qtree_module.f90
+++ src/Qtree_module.f90
@@ -1,37 +1,4 @@
 
-module Qtree_data
-    use point_module
-    use polygon_module
-    implicit none
-    integer :: num_node, num_elem, counter
-    integer :: num_intrsc_pts
-    integer :: debug_zhl
-    integer :: mate_zhl
-    integer, allocatable :: elm_typ_ma(:,:)
-    !integer :: num_mat_sets
-    type(point), allocatable :: nodes(:), temp_nodes(:), sc_nodes(:)
-    integer, allocatable :: elements(:,:)
-    logical, allocatable :: nodes_mask(:)
-    
-    
-    contains
-        subroutine QtrDataReset()
-            implicit none
-            integer :: istat
-            num_node = 0
-            num_elem = 0
-            num_intrsc_pts = 0
-            counter = 0
-            mate_zhl = 0
-            !!elm_typ = 0
-            
-            deallocate(elm_typ_ma, nodes, elements, temp_nodes, &
-                       nodes_mask,  stat=istat)
-             
-        end subroutine
-    
-end module
-    
 module Qtree_input
     use polygon_module
     use point_module
