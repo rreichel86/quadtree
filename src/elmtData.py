5162a5af38c62061c327c1afd8b491e47f6ca76d
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2023-10-13 00:40:33 +0200

    add MeshData class

--- src/elmtData.py
+++ src/elmtData.py
@@ -44,3 +44,47 @@ class Node_2d:
     def coords(self):
         return [self._x, self._y]
 
+class MeshData:
+    def __inti__(self):
+        self._list_of_nodes = []
+        self._list_of_elements = []
+        load_elements()
+        load_nodes()
+
+    def load_elements(self):
+        # Read elements from a file
+        # nro, nnodes, mat_nro, nodes
+        with open('./Output/mesh/selm.txt') as elmt_data:
+            lines = elmt_data.readlines()
+            for line in lines:
+                elmt = list(map(int, line.split()))
+                id, number_of_nodes, material_set_id, *node_ids = elmt
+                node_ids = node_ids[:number_of_nodes]
+                self._list_of_elements.append( Element(id, number_of_nodes, material_set_id, node_ids) )
+
+    def load_nodes(self):
+        # Read nodes from a file
+        # nro, x-coord, y-coord
+        with open('./Output/mesh/scor.txt') as node_data:
+            lines = node_data.readlines()
+            for line in lines:
+                first, *rest = line.split()
+                id = int(first)
+                coords = list(map(float, rest))
+                self._list_of_nodes.append( Node_2d(id, *coords) )
+
+    @property
+    def nodes(self):
+        return self._list_of_nodes
+
+    @property
+    def elements(self):
+        return self._list_of_elements
+
+    @property
+    def number_of_nodes(self):
+        return len(self._list_of_nodes)
+
+    @property
+    def number_of_elements(self):
+        return len(self._list_of_elements)
