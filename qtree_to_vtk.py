944e2f6e39ef5b254c92c6a45d79e14185e15083
Rainer Ernst Reichel <rainer.reichel@rwth-aachen.de>
2023-10-13 14:36:13 +0200

    clean up

--- qtree_to_vtk.py
+++ qtree_to_vtk.py
@@ -16,13 +16,7 @@ elements_filename = './Output/mesh/selm.txt'
 
 # Load qtree mesh data
 qtreeData = MeshData(nodes_filename, elements_filename)
-nodes = qtreeData.nodes
-elements = qtreeData.elements
-number_of_nodes = qtreeData.number_of_nodes
-number_of_elements = qtreeData.number_of_elements
-
 qtree_vtk_dataset = qtreeData.create_vtk_object()
-
 writer = vtk.vtkXMLUnstructuredGridWriter()
 writer.SetFileName(vtk_filename)
 writer.SetInputData(qtree_vtk_dataset)
